ad90bf100ffeaafadd2310b20b5c6df3202a4e5c
Emerson Bilieri Cluadelino <ebilieri@hotmail.com>
2019-05-09 19:51:30 -0300

    Encoding UTF-8

--- config/server.js
+++ config/server.js
@@ -7,7 +7,7 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views','./app/views');
 
-app.use(bodyParser.urlencoded({extend: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // mapeamento das rotas
 consign()
