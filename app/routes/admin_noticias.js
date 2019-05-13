51b6f4864939a13939cd5947543b8c5ec36dcbf1
Emerson Bilieri Cluadelino <ebilieri@hotmail.com>
2019-05-12 21:58:10 -0300

    Refactoring do projeto prático portal de notícias com Express Validator
    Refactoring do projeto prático portal de notícias com Express Validator

--- app/routes/admin_noticias.js
+++ app/routes/admin_noticias.js
@@ -5,7 +5,25 @@ module.exports = function(application){
 
 	application.post('/noticias/salvar', function(req, res){
 		var noticia = req.body;
+		console.log(noticia);
+
+		// custom module para validação de data
+		var isValidDate = require('../custom/isValidDate');
+		req.assert('titulo', 'Título é obrigatório').notEmpty();
+		req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+		req.assert('resumo', 'Resumo deve conter entre 10 e 100 caractres').len(10, 100);
+		req.assert('autor', 'Autor é obrigatório').notEmpty();
+		req.assert('data_noticia', 'Data é obrigatório').notEmpty();
+		req.assert('data_noticia').custom(isValidDate()).withMessage('A data é inválida');
+		req.assert('noticia', 'Notícia é obrigatório').notEmpty();
 		
+		var errors = req.validationErrors();
+		console.log(errors);
+		if (errors){
+			res.render('admin/form_add_noticia');
+			return;
+		}
+
 		var connection = application.config.dbConnection();
 		var noticiasModel = new application.app.models.NoticiasDAO(connection);
 
@@ -14,3 +32,8 @@ module.exports = function(application){
 		});	
 	});
 };	
+
+
+
+
+
--- config/server.js
+++ config/server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 
 var app = express();
+
 app.set('view engine', 'ejs');
 app.set('views','./app/views');
 
