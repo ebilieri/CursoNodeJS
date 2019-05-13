ce9d60316a3e80ca03085420349a755a8480733e
Emerson Bilieri Cluadelino <ebilieri@hotmail.com>
2019-05-12 22:42:40 -0300

    Mantendo o preenchimento do formulário
    Apresentando as mensagens de erro no formulário
    Mantendo o preenchimento do formulário

--- app/routes/admin_noticias.js
+++ app/routes/admin_noticias.js
@@ -1,6 +1,6 @@
 module.exports = function(application){
 	application.get('/formulario_inclusao_noticia', function(req, res){
-		res.render("admin/form_add_noticia");
+		res.render("admin/form_add_noticia", {validacao : {}, noticia : {}});
 	});
 
 	application.post('/noticias/salvar', function(req, res){
@@ -9,6 +9,7 @@ module.exports = function(application){
 
 		// custom module para validação de data
 		var isValidDate = require('../custom/isValidDate');
+		   
 		req.assert('titulo', 'Título é obrigatório').notEmpty();
 		req.assert('resumo', 'Resumo é obrigatório').notEmpty();
 		req.assert('resumo', 'Resumo deve conter entre 10 e 100 caractres').len(10, 100);
@@ -20,7 +21,7 @@ module.exports = function(application){
 		var errors = req.validationErrors();
 		console.log(errors);
 		if (errors){
-			res.render('admin/form_add_noticia');
+			res.render('admin/form_add_noticia', {validacao : errors, noticia : noticia});
 			return;
 		}
 
